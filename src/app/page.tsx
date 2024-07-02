f2bf99e2262077f2f362e50cc5eaac1ea7b6f1b9
Raymond Feng <enjoyjava@gmail.com>
2024-07-02 07:08:19 -0700

    feat: implement blink rendering
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,156 @@
+"use client";
+
+import { url } from "inspector";
 import Image from "next/image";
+import { useState, useEffect, FormEvent, ChangeEvent } from "react";
+
+type Action = {
+  label: string;
+  href: string;
+  parameters?: Parameter[];
+};
+
+type Parameter = {
+  name: string;
+  label: string;
+};
+
+type ActionMetadata = {
+  icon: string;
+  label: string;
+  title: string;
+  description: string;
+  links: {
+    actions: Action[];
+  };
+};
+
+type Props = ActionMetadata & { url: string };
+
+type ActionPostResponse = {
+  /** base64 encoded serialized transaction */
+  transaction: string;
+  /** describes the nature of the transaction */
+  message?: string;
+};
 
 export default function Home() {
+  const [url, setUrl] = useState(
+    "https://tiplink.io/api/blinks/donate?dest=GDQtcVPfjcWfrnNWjCCe7pP3ds55t1WRn83wppXs6Ntw"
+  );
+  const [data, setData] = useState<ActionMetadata | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetchUrl();
+  };
+
+  const fetchUrl = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(url, {
+        headers: { Accept: "application/json" },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data: ActionMetadata = await response.json();
+      console.log("Data: %O", data);
+      setData(data);
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleUrlChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value);
+  };
+
+  const props: Props = { ...data!, url };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="App">
+      <h1>Fetch a Solana action and render the blink</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Action URL:
+          <input type="url" value={url} onChange={handleUrlChange} size={100} />
+        </label>
+        <button type="submit">Fetch</button>
+      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {data && <ActionForm {...props} />}
+    </div>
   );
 }
+
+const ActionForm = (data: Props) => {
+  const [input, setInput] = useState("");
+  const handleButtonClick = async (href: string) => {
+    // Implement the logic to handle donation click
+    // This could involve redirecting the user to the href or making an API call
+    console.log(`Redirecting to: ${href}`);
+    href = new URL(href, data.url).toString();
+    const res = await fetch(href, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        account: "GrHyYxxFYPtzGyL1eJFJPQEmXVuw7dLHZye8xQU3z6xQ",
+      }),
+    });
+    const tx: ActionPostResponse = await res.json();
+    console.log(`Transaction: %O`, tx);
+    alert(`Transaction: ${tx.transaction}`);
+  };
+
+  const renderActionButton = (action: Action) => {
+    if (action.parameters) {
+      const name = action.parameters[0].name;
+      return (
+        <div key={action.label}>
+          <label>{action.parameters[0].label}: </label>
+          <input type="text" onChange={(e) => setInput(e.target.value)} />
+          <button
+            key={action.label}
+            onClick={(e) => {
+              handleButtonClick(action.href.replace(`{${name}}`, input));
+            }}
+          >
+            {action.label}
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <button
+          key={action.label}
+          onClick={() => handleButtonClick(action.href)}
+        >
+          {action.label}
+        </button>
+      );
+    }
+  };
+
+  const buttons = data.links?.actions?.length
+    ? data.links.actions.map(renderActionButton)
+    : renderActionButton({
+        label: data.label,
+        href: data.url,
+      });
+
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+      <Image src={data.icon} alt={data.label} height="400" width="400" />
+      <div>{buttons}</div>
+    </div>
+  );
+};
